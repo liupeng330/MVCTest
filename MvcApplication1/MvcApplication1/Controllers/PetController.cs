e416aa07e3cf80e13388f278217959fb931dffd2
liupeng330 <330liupeng@gmail.com>
2014-01-13 22:09:48 +0800

    commit first test mvc

---
+++ MvcApplication1/MvcApplication1/Controllers/PetController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication1.Models;
+using MvcApplication1.Models.BIZ;
+
+namespace MvcApplication1.Controllers
+{
+    public class PetController : Controller
+    {
+        public ActionResult PictureUpload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult PictureUpload(PictureModel model)
+        {
+            if (model.PictureFile.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(model.PictureFile.FileName);
+                var filePath = Server.MapPath("/Content/Uploads");
+                string savedFileName = Path.Combine(filePath, fileName);
+                model.PictureFile.SaveAs(savedFileName);
+                PetManagement.CreateThumbnail(fileName, filePath, 100, 100, true);
+            }
+            return View(model);
+        }
+    }
+}
---
+++ MvcApplication1/MvcApplication1/Models/PictureModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class PictureModel
+    {
+        [Required]
+        public HttpPostedFileBase PictureFile { get; set; }
+    }
+}
